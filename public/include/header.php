252bd1bff509f12d386f51681f794fb31225a3f5
Felix Angell <mail@felixangell.com>
2015-07-07 02:57:49 +0100

    tweaks

--- public/include/header.php
+++ public/include/header.php
@@ -18,10 +18,10 @@
         <h3>Ark Resources</h3>
         <nav class="link-list">
             <ul>
-                <li><a href="reference.php">Reference</a></li>
+                <li><a href="reference.php">Reference (nightly)</a></li>
                 <li><a href="styleguide.php">Style Guide</a></li>
-                <li><a target="_tab" href="//github.com/ark-lang/ark-docs">All Documentation</a></li>
                 <li><a href="contributing.php">Contributor Guide</a></li>
+                <li><a target="_tab" href="//github.com/ark-lang/ark-docs">All Documentation</a></li>
             </ul>
         </nav>
     </div>
